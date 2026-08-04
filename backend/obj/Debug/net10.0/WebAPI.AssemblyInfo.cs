4d80b0e30d10e6b4abace7067bf81338daca6426
Zeynep Tuğçe Keskin <ztugcekeskin@gmail.com>
2026-08-04 16:48:42 +0300

    updated

--- backend/obj/Debug/net10.0/WebAPI.AssemblyInfo.cs
+++ backend/obj/Debug/net10.0/WebAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3684a657832b4de5323a3d50e1358a9ecdce355")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dbc9fdc25b6bfda067aaa3263ac571b72a1b66f6")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
