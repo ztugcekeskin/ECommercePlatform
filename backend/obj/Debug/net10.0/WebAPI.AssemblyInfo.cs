294dbcb17a5418fc68015c0efd2d0d45fdfea660
Zeynep Tuğçe Keskin <ztugcekeskin@gmail.com>
2026-08-13 11:19:18 +0300

    update product and review features

--- backend/obj/Debug/net10.0/WebAPI.AssemblyInfo.cs
+++ backend/obj/Debug/net10.0/WebAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+283c2b7ec1392baba4abafd33913c58ec24c3ea2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94d19ea9bc13df08502b2fd85fc87a9045333116")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
